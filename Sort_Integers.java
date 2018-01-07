060431864b92781d30ae8a27fd842e5260b07948
Tyton25 <tybenz1@gmail.com>
2018-01-07 14:39:35 -0700

    Update Sort_Integers.java

--- Sort_Integers.java
+++ Sort_Integers.java
@@ -16,10 +16,7 @@ public class Main {
 	}
 	
 	public static int[] sortIntegers(int[] array) {
-//		int[] sortedArray = new int[array.length];
-//		for(int i=0; i<array.length; i++) {
-//			sortedArray[i] = array[i];
-//		}
+		
 		int[] sortedArray = Arrays.copyOf(array, array.length);
 		boolean flag = true;
 		int temp;		
